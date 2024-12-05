42dbb4bccfdd353f6cc2265a78be6c89c358e95a
waynee95 <larsericmarquardt@mail.de>
2024-12-05 23:07:34 +0100

    Day 1 done

--- 01/Day01.hs
+++ 01/Day01.hs
@@ -1,37 +1,24 @@
-import Data.Char (digitToInt, isDigit)
-import Data.List (isPrefixOf, tails)
-import Data.Maybe (mapMaybe)
+import Data.List (sort)
+import Data.Map qualified as M
+import System.Environment (getArgs)
 import Text.Printf (printf)
 
-calibrationValue :: [Int] -> Int
-calibrationValue xs = head xs * 10 + last xs
+part1 :: ([Int], [Int]) -> Int
+part1 (xs, ys) = sum $ zipWith (\x y -> abs (x - y)) (sort xs) (sort ys)
 
-digitify :: String -> Maybe Int
-digitify s
-    | ("1", "one") `eitherIsPrefixOf` s = Just 1
-    | ("2", "two") `eitherIsPrefixOf` s = Just 2
-    | ("3", "three") `eitherIsPrefixOf` s = Just 3
-    | ("4", "four") `eitherIsPrefixOf` s = Just 4
-    | ("5", "five") `eitherIsPrefixOf` s = Just 5
-    | ("6", "six") `eitherIsPrefixOf` s = Just 6
-    | ("7", "seven") `eitherIsPrefixOf` s = Just 7
-    | ("8", "eight") `eitherIsPrefixOf` s = Just 8
-    | ("9", "nine") `eitherIsPrefixOf` s = Just 9
-    | otherwise = Nothing
+part2 :: ([Int], [Int]) -> Int
+part2 (xs, ys) = sum . map (uncurry (*)) . M.toList $ M.intersectionWith (*) m1 m2
   where
-    eitherIsPrefixOf (s1, s2) s = s1 `isPrefixOf` s || s2 `isPrefixOf` s
-
-part1 :: [String] -> Int
-part1 = sum . map (calibrationValue . map digitToInt . filter isDigit)
-
-part2 :: [String] -> Int
-part2 = sum . map (calibrationValue . mapMaybe digitify . tails)
+    m1 = M.fromListWith (+) $ map (,1) xs
+    m2 = M.fromListWith (+) $ map (,1) ys
 
 main :: IO ()
 main = do
-    l <- lines <$> readFile "input.txt"
-    printf "Part 1: %d\n" $ part1 l
-    printf "Part 2: %d\n" $ part2 l
+    [filename] <- getArgs
+    ls <- lines <$> readFile filename
+    let (xs, ys) = unzip . map ((\[x, y] -> (read x, read y)) . words) $ ls
+    printf "Part 1: %d\n" $ part1 (xs, ys)
+    printf "Part 2: %d\n" $ part2 (xs, ys)
 
--- Part 1: 57346
--- Part 2: 57345
+-- Part 1: 2285373
+-- Part 2: 21142653
